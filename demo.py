70ecba267d7378340f0a56c1ee3abed046e9a79f
smoran3 <smoran@dvrpc.org>
2023-09-18 11:53:25 -0400

    fixed!

--- demo.py
+++ demo.py
@@ -1,12 +1,15 @@
 from plan_belt.conflation import conflator
 
+# ensure all geometry fields are called "geom" and objectid fields are lowercase
+# conflation results are in postgres schema "rejoined"
+
 conflator(
     "speed_limit",  # name of your database
     "localhost",  # name of your config file in pg_etl config file
     "inrix_2021",  # name of input table (what you want to conflate)
     "inrix",  # what you want the output table to be named
     "objectid",  # uid of conflate layer
-    "OBJECTID",  # uid of base layer
+    "objectid",  # uid of base layer
     "typologies",  # base layer that you want to conflate to
     "b.REFSPDMEAN, b.REFSPDMIN, b.REFSPDMAX",  # columns from conflate layer you want to include. if multiple, just use b. for each. like 'b.line, b.geom'
     5,  # distance threshold in meters. 5 is a good starting point but might need to tune
--- scripts/data_setup.py
+++ scripts/data_setup.py
@@ -23,62 +23,40 @@ from sqlalchemy_utils import database_exists, create_database
 import env_vars as ev
 from env_vars import GIS_ENGINE, ENGINE
 
-# # read provided shapefiles and write to postgres
-# inrix = gpd.read_file(rf"{ev.DATA_ROOT}\shapefiles\{inrix_filename}.shp")
-# inrix.to_postgis(rf"inrix{inrix_year}", con=ENGINE, if_exists="replace")
 
 typologies = gpd.read_file(rf"{ev.DATA_ROOT}\shapefiles\{typologies_filename}.shp")
-# rename geometry column
-# typologies = typologies.rename_geometry("geom", inplace=True)
-
 typologies.to_postgis("typologies", con=ENGINE, if_exists="replace")
 
 # rename the geometry column to geom so it works with conflation tool
 Q_rename = """
     ALTER TABLE typologies
     RENAME COLUMN geometry to geom;
+    ALTER TABLE typologies
+    RENAME COLUMN "OBJECTID" to objectid;
+    COMMIT;
 """
 from sqlalchemy import text
 
 with ENGINE.connect() as conn:
     result = conn.execute(text(Q_rename))
 
-# # filter inrix to just philadelphia
-# Q_PhilaInrix = rf"""
-#     select *, st_transform(geometry, 4326) as geom
-#     from (
-#         select *
-#         from inrix{inrix_year} i
-#         where i."County" = 'PHILADELPHIA'
-#     ) foo
-# """
-# phila_inrix = gpd.GeoDataFrame.from_postgis(
-#     Q_PhilaInrix,
-#     con=ENGINE,
+# # read inrix from GIS database
+# inrix = gpd.GeoDataFrame.from_postgis(
+#     """select objectid, segid, roadname, county, startlat, startlong, endlat, endlong, refspdmean, refspdmin, refspdmax, shape as geom
+# from transportation.cmp2021_inrix_traveltimedata cit
+# where cit.county = 'PHILADELPHIA' """,
+#     con=GIS_ENGINE,
 #     geom_col="geom",
 # )
-
-
 # # write to postgis
-# phila_inrix.to_postgis(rf"phila_inrix_{inrix_year}", con=ENGINE, if_exists="replace")
-
-# read inrix from GIS database
-inrix = gpd.GeoDataFrame.from_postgis(
-    """select objectid, segid, roadname, county, startlat, startlong, endlat, endlong, refspdmean, refspdmin, refspdmax, shape as geom 
-from transportation.cmp2021_inrix_traveltimedata cit
-where cit.county = 'PHILADELPHIA' """,
-    con=GIS_ENGINE,
-    geom_col="geom",
-)
-# write to postgis
-inrix.to_postgis("inrix_2021", con=ENGINE, if_exists="replace")
+# inrix.to_postgis("inrix_2021", con=ENGINE, if_exists="replace")
 
 
-# read HIN from GIS database
-hin = gpd.GeoDataFrame.from_postgis(
-    """SELECT * FROM transportation.philly_highinjurynetwork p""",
-    con=GIS_ENGINE,
-    geom_col="shape",
-)
-# write to postgis
-hin.to_postgis("hin", con=ENGINE, if_exists="replace")
+# # read HIN from GIS database
+# hin = gpd.GeoDataFrame.from_postgis(
+#     """SELECT * FROM transportation.philly_highinjurynetwork p""",
+#     con=GIS_ENGINE,
+#     geom_col="shape",
+# )
+# # write to postgis
+# hin.to_postgis("hin", con=ENGINE, if_exists="replace")
