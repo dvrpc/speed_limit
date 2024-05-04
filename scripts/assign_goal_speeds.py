9cbd6f36ab557d52dccee0d0dcfaa095bd3722ee
smoran3 <smoran@dvrpc.org>
2024-05-04 06:29:19 -0400

    update urban core query

--- scripts/assign_goal_speeds.py
+++ scripts/assign_goal_speeds.py
@@ -50,118 +50,126 @@ meta = MetaData()
 meta.reflect(bind=ENGINE)
 T = meta.tables["typologies_joined"]
 ## update queries
-# bike mix
-b1 = update(T).values(bike_mix="high").where(T.c.type == "Bus Bike Lane")
-b2 = update(T).values(bike_mix="high").where(T.c.type == "Sharrow")
-b3 = update(T).values(bike_mix="high").where(T.c.type == None)
 
-b4 = update(T).values(bike_mix="mod").where(T.c.type == "Conventional w Sharrows")
-b5 = update(T).values(bike_mix="mod").where(T.c.type == "Conventional")
-b6 = update(T).values(bike_mix="mod").where(T.c.type == "Paint Buffered")
-b7 = (
-    update(T)
-    .values(bike_mix="mod")
-    .where(T.c.type == "Two Way Unprotected Bicycle Lane")
-)
+# # bike mix
+# b1 = update(T).values(bike_mix="high").where(T.c.type == "Bus Bike Lane")
+# b2 = update(T).values(bike_mix="high").where(T.c.type == "Sharrow")
+# b3 = update(T).values(bike_mix="high").where(T.c.type == None)
 
-b8 = (
-    update(T).values(bike_mix="low").where(T.c.type == "One Way Protected Bicycle Lane")
-)
+# b4 = update(T).values(bike_mix="mod").where(T.c.type == "Conventional w Sharrows")
+# b5 = update(T).values(bike_mix="mod").where(T.c.type == "Conventional")
+# b6 = update(T).values(bike_mix="mod").where(T.c.type == "Paint Buffered")
+# b7 = (
+#     update(T)
+#     .values(bike_mix="mod")
+#     .where(T.c.type == "Two Way Unprotected Bicycle Lane")
+# )
 
-statements = [b1, b2, b3, b4, b5, b6, b7, b8]
-for s in statements:
-    conn.execute(s)
-    conn.commit()
+# b8 = (
+#     update(T).values(bike_mix="low").where(T.c.type == "One Way Protected Bicycle Lane")
+# )
 
-# ped mix
-s1 = update(T).values(ped_mix="high").where(T.c.sw_ratio < 0.45)
-s2 = (
-    update(T)
-    .values(ped_mix="mod")
-    .where(T.c.sw_ratio >= 0.45)
-    .where(T.c.sw_ratio <= 0.82)
-)
-s3 = update(T).values(ped_mix="low").where(T.c.sw_ratio > 0.82)
+# statements = [b1, b2, b3, b4, b5, b6, b7, b8]
+# for s in statements:
+#     conn.execute(s)
+#     conn.commit()
 
-statements = [s1, s2, s3]
-for s in statements:
-    conn.execute(s)
-    conn.commit()
+# # ped mix
+# s1 = update(T).values(ped_mix="high").where(T.c.sw_ratio < 0.45)
+# s2 = (
+#     update(T)
+#     .values(ped_mix="mod")
+#     .where(T.c.sw_ratio >= 0.45)
+#     .where(T.c.sw_ratio <= 0.82)
+# )
+# s3 = update(T).values(ped_mix="low").where(T.c.sw_ratio > 0.82)
+
+# statements = [s1, s2, s3]
+# for s in statements:
+#     conn.execute(s)
+#     conn.commit()
 
 
-# circuit overwrite: if roadway is adjacent to circuit trail, bike/ped measures are overwritten to low/low mixing
-c1 = update(T).values(bike_mix="low").where(T.c.circuit == "Existing")
-c2 = update(T).values(ped_mix="low").where(T.c.circuit == "Existing")
-statements = [c1, c2]
-for s in statements:
-    conn.execute(s)
-    conn.commit()
+# # circuit overwrite: if roadway is adjacent to circuit trail, bike/ped measures are overwritten to low/low mixing
+# c1 = update(T).values(bike_mix="low").where(T.c.circuit == "Existing")
+# c2 = update(T).values(ped_mix="low").where(T.c.circuit == "Existing")
+# statements = [c1, c2]
+# for s in statements:
+#     conn.execute(s)
+#     conn.commit()
 
-# overall modal mix
-m1 = (
-    update(T)
-    .values(modal_mix="high")
-    .where(T.c.bike_mix == "high")
-    .where(T.c.ped_mix == "high")
-)
-m2 = (
-    update(T)
-    .values(modal_mix="high")
-    .where(T.c.bike_mix == "mod")
-    .where(T.c.ped_mix == "high")
-)
-m3 = (
-    update(T)
-    .values(modal_mix="high")
-    .where(T.c.bike_mix == "high")
-    .where(T.c.ped_mix == "mod")
-)
-m4 = (
-    update(T)
-    .values(modal_mix="mod")
-    .where(T.c.bike_mix == "mod")
-    .where(T.c.ped_mix == "mod")
-)
-m5 = (
-    update(T)
-    .values(modal_mix="mod")
-    .where(T.c.bike_mix == "high")
-    .where(T.c.ped_mix == "low")
-)
-m6 = (
-    update(T)
-    .values(modal_mix="mod")
-    .where(T.c.bike_mix == "low")
-    .where(T.c.ped_mix == "high")
-)
-m7 = (
-    update(T)
-    .values(modal_mix="low")
-    .where(T.c.bike_mix == "low")
-    .where(T.c.ped_mix == "low")
-)
-m8 = (
-    update(T)
-    .values(modal_mix="low")
-    .where(T.c.bike_mix == "mod")
-    .where(T.c.ped_mix == "low")
-)
-m9 = (
-    update(T)
-    .values(modal_mix="low")
-    .where(T.c.bike_mix == "low")
-    .where(T.c.ped_mix == "mod")
-)
-statements = [m1, m2, m3, m4, m5, m6, m7, m8, m9]
-for s in statements:
-    conn.execute(s)
-    conn.commit()
+# # overall modal mix
+# m1 = (
+#     update(T)
+#     .values(modal_mix="high")
+#     .where(T.c.bike_mix == "high")
+#     .where(T.c.ped_mix == "high")
+# )
+# m2 = (
+#     update(T)
+#     .values(modal_mix="high")
+#     .where(T.c.bike_mix == "mod")
+#     .where(T.c.ped_mix == "high")
+# )
+# m3 = (
+#     update(T)
+#     .values(modal_mix="high")
+#     .where(T.c.bike_mix == "high")
+#     .where(T.c.ped_mix == "mod")
+# )
+# m4 = (
+#     update(T)
+#     .values(modal_mix="mod")
+#     .where(T.c.bike_mix == "mod")
+#     .where(T.c.ped_mix == "mod")
+# )
+# m5 = (
+#     update(T)
+#     .values(modal_mix="mod")
+#     .where(T.c.bike_mix == "high")
+#     .where(T.c.ped_mix == "low")
+# )
+# m6 = (
+#     update(T)
+#     .values(modal_mix="mod")
+#     .where(T.c.bike_mix == "low")
+#     .where(T.c.ped_mix == "high")
+# )
+# m7 = (
+#     update(T)
+#     .values(modal_mix="low")
+#     .where(T.c.bike_mix == "low")
+#     .where(T.c.ped_mix == "low")
+# )
+# m8 = (
+#     update(T)
+#     .values(modal_mix="low")
+#     .where(T.c.bike_mix == "mod")
+#     .where(T.c.ped_mix == "low")
+# )
+# m9 = (
+#     update(T)
+#     .values(modal_mix="low")
+#     .where(T.c.bike_mix == "low")
+#     .where(T.c.ped_mix == "mod")
+# )
+# statements = [m1, m2, m3, m4, m5, m6, m7, m8, m9]
+# for s in statements:
+#     conn.execute(s)
+#     conn.commit()
+
 
 ### overlay urban core
 # read from db
 tbl = gpd.GeoDataFrame.from_postgis(
-    """select tj.*, st_intersects(tj.geom, u.geom ) as uc_overlap
-        from typologies_joined tj, urbancore u""",
+    """select tj.*, foo.st_intersects as uc_overlap
+        from typologies_joined tj
+        inner join (
+            select distinct tj.objectid, st_intersects(tj.geom, u.geom)
+            from typologies_joined tj 
+            left join urbancore u 
+            on st_intersects(tj.geom, u.geom)) foo
+        on tj.objectid = foo.objectid""",
     con=ENGINE,
     geom_col="geom",
 )
