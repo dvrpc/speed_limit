255051fb1150348d37596c4e1bb1329940753835
smoran3 <smoran@dvrpc.org>
2024-05-31 20:51:43 -0400

    notes in scratch pad

--- scripts/scratch_pad.sql
+++ scripts/scratch_pad.sql
@@ -1,66 +1,27 @@
--- obs_greaterthan_posted_overnight
--- any hour netween 7pm and 12am
-select objectid, "ST_RT_NO" , "SPD_LIMIT" , "TYPOLOGY" , "HIN" , geom , spdwkd1920, spdwkd2021, spdwkd2122, spdwkd2223, spdwkd2300
-from rejoined.inrix i 
-where spdwkd1920 > "SPD_LIMIT"
-or spdwkd2021 > "SPD_LIMIT"
-or spdwkd2122 > "SPD_LIMIT"
-or spdwkd2223 > "SPD_LIMIT"
-or spdwkd2300 > "SPD_LIMIT"
-
-
-
-with tblA as(
+-- to identify mismatches in default speed across divided roads
+--at first glance in Q, these results look reasonable, but digging deeper, there are sometimes matched pairs with duplicates on top with differnt assigned speeds
+--need to figure out where these duplicates are coming from
+with tblA AS(
 	select *
-	from rejoined.inrix i
-	where spdwkd1920 > "SPD_LIMIT"
-	),
+	from typologies_joined tj 
+	where "SEQ_NO" in(
+		select "SEQ_NO"
+		from (
+		select "ST_RT_NO","SEQ_NO" , count(*) as cnt
+		from typologies_joined tj 
+		where "DIV_RDWY" = 1
+		group by "ST_RT_NO", "SEQ_NO"
+		having count(*) > 1) foo)
+	and "DIV_RDWY"= 1
+),
 tblB as(
-	select *
-	from rejoined.inrix i2
-	where spdwkd2021 > "SPD_LIMIT"
-	),
-tblC as(
-	select *
-	from rejoined.inrix i3
-	where spdwkd2122 > "SPD_LIMIT"
-	),
-tblD as(
-	select *
-	from rejoined.inrix i4
-	where spdwkd2223 > "SPD_LIMIT"
-	),
-tblE as(
-	select *
-	from rejoined.inrix i5
-	where spdwkd2300 > "SPD_LIMIT"
-	),
-tblF as(
-	select *
-	from tblA
-	union all
-	select *
-	from tblB
-	union all
-	select *
-	from tblC
-	union all
-	select*
-	from tblD
-	union all
-	select *
-	from tblE
-	),
-tblCOUNT as(
-	select objectid, count(*), round(count(*)/5::numeric, 2) as percentage
-	from tblF
-	group by (objectid)
-	)
-select i6.*, tc.count, tc.percentage
-from rejoined.inrix i6 
-left join tblCOUNT tc
-on i6.objectid = tc.objectid
-
-
--- "ST_RT_NO" , "SPD_LIMIT" , "TYPOLOGY" , "HIN" , geom ,
---spdwkd1920, spdwkd2021, spdwkd2122, spdwkd2223, spdwkd2300
+select "ST_RT_NO" , "SEQ_NO", default_speed, count(*)
+from tblA
+group by "ST_RT_NO" , "SEQ_NO", default_speed 
+having count(*) = 1
+)
+select b.*, a.geom
+from tblB b
+inner join tbla a
+on a."ST_RT_NO" = b."ST_RT_NO"
+and a."SEQ_NO" = b. "SEQ_NO"
